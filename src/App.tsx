f0d87ad13080238f58502bc1c1afc11fc889247f
Kevin Jennison <kevin.jennison1@gmail.com>
2024-12-11 16:21:50 -0500

    Repro

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { PolotnoContainer, SidePanelWrap, WorkspaceWrap } from "polotno";
+import { Workspace } from "polotno/canvas/workspace";
+import { createDemoApp } from "polotno/polotno-app";
+import { SidePanel } from "polotno/side-panel";
+import { Toolbar } from "polotno/toolbar/toolbar";
+import { ZoomButtons } from "polotno/toolbar/zoom-buttons";
+
+const { store } = createDemoApp({
+  container: document.getElementById("root"),
+  key: "ucd7jgX3xMroM2hBxa4W",
+  sections: [],
+  // you can hide back-link on a paid license
+  showCredit: true,
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <link
+        href="https://unpkg.com/@blueprintjs/core@5/lib/css/blueprint.css"
+        rel="stylesheet"
+      />
+      <PolotnoContainer style={{ height: "100vh", width: "100vw" }}>
+        <SidePanelWrap>
+          <SidePanel store={store} />
+        </SidePanelWrap>
+        <WorkspaceWrap>
+          <Toolbar downloadButtonEnabled store={store} />
+          <Workspace store={store} />
+          <ZoomButtons store={store} />
+        </WorkspaceWrap>
+      </PolotnoContainer>
     </div>
   );
 }
