66a4260b35058e22d82156467f3320b2642d455c
Kevin Jennison <kevin.jennison1@gmail.com>
2024-12-12 12:00:47 -0500

    Don't use createDemoApp

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
 import React from "react";
 import { PolotnoContainer, SidePanelWrap, WorkspaceWrap } from "polotno";
 import { Workspace } from "polotno/canvas/workspace";
-import { createDemoApp } from "polotno/polotno-app";
 import { SidePanel } from "polotno/side-panel";
 import { Toolbar } from "polotno/toolbar/toolbar";
 import { ZoomButtons } from "polotno/toolbar/zoom-buttons";
+import { createStore } from "polotno/model/store";
 
-const { store } = createDemoApp({
-  container: document.getElementById("root"),
-  key: "ucd7jgX3xMroM2hBxa4W",
-  sections: [],
+const store = createStore({
+  key: "key",
   // you can hide back-link on a paid license
   showCredit: true,
 });
+store.addPage();
 
 function App() {
   return (
